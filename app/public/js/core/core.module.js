46430e14b5876fa2716e50b65f9a780a9a0ab411
Laurent Dezitter <laurent.dezitter@gmail.com>
2016-12-24 13:28:54 +0100

    move filters to filter/

--- app/public/js/core/core.module.js
+++ app/public/js/core/core.module.js
@@ -3,8 +3,8 @@ const angular = require('angular');
 const ActionModule = require('./action/action.module');
 const MovieModule = require('./movie/movie.module');
 const StoreModule = require('./store/store.module');
-const JoinFilterFactory = require('./join-filter/join-filter.factory');
-const PadFilterFactory = require('./pad-filter/pad-filter.factory');
+const JoinFilterFactory = require('./filter/join-filter/join-filter.factory');
+const PadFilterFactory = require('./filter/pad-filter/pad-filter.factory');
 
 const coreModule = angular.module('core', [
     ActionModule.name,
